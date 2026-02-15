536f18dabaf36844a1b819b0edd94a4190a4da5d
davidvm419-tech <davidvm419@outlook.com>
2026-02-15 17:17:13 -0500

    added updated readme and updated company name and vission

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# coding-project-template
+# Developing Front-End Apps with React final project
+
+## Houseplant sopping App
+repo location: https://github.com/davidvm419-tech/e-plantShopping.git
+
+Rooted Home is a React + Redux shopping app where users can browse houseplants, add them to a cart, update quantities, and view totals in real time. This project represents my approach to the final assignment for the IBM Front-End React course.
+
+Feel free to check it out.
--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,25 +5,24 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Rooted Home, where you will bring Nature to Home!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        At Rooted Home, a simple idea turned into a cozy online plant shop. 
+        We're not trying to be botanical geniuses or jungle explorers 
+        we just believe that adding a bit of green can make any space feel calmer, brighter, and more alive.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+      Rooted Home was created for people who want plants without the stress. 
+      Whether you're a total beginner or someone who just wants their home to feel a little more natural, 
+      we've got you covered. Our collection focuses on easy to love houseplants, 
+      clean design, and a smooth shopping experience that doesn't require you to know every Latin name in the book.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+      At Rooted Home, our mission is simple:
+      Bring nature closer to you, one plant at a time, with a sence of good vibes and greener spaces.
       </p>
     </div>
   );
